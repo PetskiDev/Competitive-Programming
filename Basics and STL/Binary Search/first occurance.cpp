26588563d14a3682050094f805ceec14e945604d
PetarKovacovski <petar.kovacovski@gmail.com>
2022-03-09 22:09:59 +0100

    Create first occurance.cpp

---
+++ Basics and STL/Binary Search/first occurance.cpp
@@ -0,0 +1,25 @@
+int first(int arr[], int x, int n)
+{
+    int low = 0, high = n - 1, res = -1;
+    while (low <= high)
+    {
+         
+        // Normal Binary Search Logic
+        int mid = (low + high) / 2;
+         
+        if (arr[mid] > x)
+            high = mid - 1;
+        else if (arr[mid] < x)
+            low = mid + 1;
+ 
+        // If arr[mid] is same as x, we
+        // update res and move to the left
+        // half.
+        else
+        {
+            res = mid;
+            high = mid - 1;
+        }
+    }
+    return res;
+}
